0fcb012a3647b8aff871224e9e8904f157df99c6
SAHLA THASNIM <152372740+SahlaThasnim89@users.noreply.github.com>
2025-07-01 22:52:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import Marquee from './components/Marquee';
 import Projects from './components/Projects';
 import About from './components/About';
-// import Work from './components/Work';
+import Work from './components/Work';
 import Contact from './components/Contact';
 
 
@@ -19,7 +19,7 @@ function App() {
       <Marquee/>
       <Projects/>
       <About/>
-      {/* <Work/> */}
+       <Work/>
       <Contact/>
    </main>
   )
